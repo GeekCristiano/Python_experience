f41ee58cb7e0e711c70bf0c6efa0048cdfb97a85
Sergey S <GeekCristiano@gmail.com>
2018-02-25 18:39:55 +0300

    Add example for work with dictionary

--- Tuples.py
+++ Tuples.py
@@ -17,3 +17,10 @@ else:
         print("First is bigger than second!")
     else:
         print("Second is bigger than first.")
+
+# In dictionary each item is tuple
+a = {"x": 100, "y": 4}
+list = list(a.items())
+
+for i in list:                                               # tuples everywhere :)
+    print("Current item is: %s. Type object is: %s" % (str(i), str(type(i))))
