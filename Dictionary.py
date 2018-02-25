114db86f532710e89f098f067c3c1d2ecd278a87
Sergey S <GeekCristiano@gmail.com>
2018-02-25 23:00:41 +0300

    Add example for iterating over dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -1,5 +1,5 @@
 # Create dictionary
-dic = {"ONE": 1, "two": 2, "three": 3, "one": 2}
+dic = {"ONE": 1, "two": 2, "three": 3, "one": 2, "five": 5}
 if dic["ONE"] == dic["one"]:
     print("Keys aren't case sensitive!")
 else:
@@ -9,3 +9,15 @@ else:
 del dic["ONE"]
 dic.update({"one": 1})
 print(dic)
+
+even = {"two": 2, "four": 4, "six": 6}
+odd = {"one": 1, "three": 3, "five": 5}
+
+print(list(odd.items()))
+
+# Iterate over dictionary
+for key in even.keys():
+    if key in dic.keys():
+        print("Source dictionary does contain key %s" % key)
+    else:
+        print("Source dictionary doesn't contain key %s" % key)
