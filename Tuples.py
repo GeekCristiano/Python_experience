a023909c8265c23827acbeb18b5c8487ca855f7b
Sergey S <GeekCristiano@gmail.com>
2018-02-25 18:15:44 +0300

    Add comparison example

--- Tuples.py
+++ Tuples.py
@@ -5,3 +5,15 @@ man = ("Bob", "28", "MyCompanyName")
 print(name)
 print(age)
 print(company)
+
+tl1 = (6, 6)
+tl2 = (5, 7)
+
+# comparison of two tuples
+if (tl1 == tl2):
+    print("Equals!")
+else:
+    if (tl1 > tl2):
+        print("First is bigger than second!")
+    else:
+        print("Second is bigger than first.")
