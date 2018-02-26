515dc0b473553aa480e618f42ab21afed66db496
Sergey S <GeekCristiano@gmail.com>
2018-02-26 23:49:16 +0300

    Add example of using arithmetic operators

--- Operators.py
+++ Operators.py
@@ -1,1 +1,12 @@
 # Let's study how different operators work in the python
+
+# A few simple arithmetic operators
+x = 3
+y = 5
+print(x - y)
+print(x + y)
+print(x / y)
+print(x * y)
+print(y % x)
+print(y % y)
+print(x % y)
