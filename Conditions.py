cee3549057c8315e8db1f1b0094f9248a42a9127
Sergey S <GeekCristiano@gmail.com>
2018-02-28 23:25:58 +0300

    Add switch case analog in python

--- Conditions.py
+++ Conditions.py
@@ -17,4 +17,27 @@ comparenumbers(2, 2)
 x = 6
 y = 4
 
-print("x is greater than y" if (x>y) else "x is greater than y")
+print("x is greater than y" if (x > y) else "x is greater than y")
+
+
+# switch-case python analog
+
+def getNameMonth(number):
+    months = {1: "January",
+              2: "February",
+              3: "March",
+              4: "April",
+              5: "May",
+              6: "June",
+              7: "July",
+              8: "August",
+              9: "September",
+              10: "October",
+              11: "November",
+              12: "December"}
+
+    return months.get(number) if number in months.keys() else "Incorrect input value"
+
+print(getNameMonth(4))
+print(getNameMonth(127))
+
