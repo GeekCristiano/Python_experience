bde7ae503670787d7b9e58b195d75291ac7fe61c
Sergey S <GeekCristiano@gmail.com>
2018-03-02 00:08:26 +0300

    Add example for iterating list

--- Loops.py
+++ Loops.py
@@ -8,10 +8,27 @@ while x < 8:
     print(x)
     x += 1
 
-#  for loop example
+
+# for loop example
 
 def print_twice(value):
     for i in range(2):
         print(value)
 
+
 print_twice("String")
+
+
+# iterate over list
+def get_even_numbers(numbers):
+    even_numbers = []
+
+    for n in numbers:
+        if n % 2 == 0:
+            even_numbers.append(n)
+
+    return even_numbers
+
+
+numbers = [0, -17, 4, 17, 6, 9]
+print(get_even_numbers(numbers))
