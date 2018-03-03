2b4a08f237c333b7c56ffc1934a3681c38a76d53
Sergey S <GeekCristiano@gmail.com>
2018-03-03 18:22:53 +0300

    Adds one more example of regex

--- Regex.py
+++ Regex.py
@@ -6,3 +6,11 @@ import re
 str = "Learning python is fun and interesting."
 pattern = r"^\w+"
 print(re.findall(pattern, str))
+
+regexList = re.split(r"\s", str)
+noneRegexList = re.split(" ", str)
+
+if regexList == noneRegexList:
+    print("Lists are the same.")
+else:
+    print("Lists are the different.")
