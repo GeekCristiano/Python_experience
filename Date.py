ed23ce86cb65ac4fc6f2f28fc9cb29f2d1b24134
Sergey S <GeekCristiano@gmail.com>
2018-03-03 22:31:14 +0300

    Adds an example of using date class

--- Date.py
+++ Date.py
@@ -1,6 +1,15 @@
 # Let's study the tools for working with the date and time
+from datetime import date
 
-from datetime import date
-from datetime import date
-from datetime import time
-from datetime import timedelta
+week_days = {0: "Mon", 1: "Tue", 2: "Wed", 3: "Thu", 4: "Fri", 5: "Sat", 6: "Sun"}
+
+months = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul",
+          8: "Aug", 9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+
+# Get current date
+
+today = date.today()
+print("Current date is:", today)
+print("Current day of the week is", week_days.get(today.weekday()))
+print("Current day is:", today.day)
+print("Current month is:", months.get(today.month))
