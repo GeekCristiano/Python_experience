97de66b1ffc10a7ff0457b49460f9d9f4dda1246
Sergey S <GeekCristiano@gmail.com>
2018-03-03 18:13:12 +0300

    Adds an example of regular expression

--- Regex.py
+++ Regex.py
@@ -1,5 +1,8 @@
-
 # using regular expression in python
 
 # imported necessary module
 import re
+
+str = "Learning python is fun and interesting."
+pattern = r"^\w+"
+print(re.findall(pattern, str))
