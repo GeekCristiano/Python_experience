113b859a2be34b876c3edd6a554dceca01520460
Sergey S <GeekCristiano@gmail.com>
2018-03-04 21:19:18 +0300

    Adds example how iterate over calendar

--- Calendar.py
+++ Calendar.py
@@ -3,5 +3,18 @@ import calendar
 
 # create textCalendar for March 2018
 c = calendar.TextCalendar(calendar.MONDAY)
+# if you want to get HTML code of calendar use HTMLCalendar class
+# c = calendar.HTMLCalendar(calendar.MONDAY)
 str = c.formatmonth(2018, 3)
 print(str)
+
+# iterate over calendar
+for i in c.itermonthdates(2018, 3):
+    print(i)
+
+# print name of months and  week days
+for m in calendar.month_name:
+    print(m)
+
+for d in calendar.day_abbr:
+    print(d)
