8da2e09278b65d2c89da2a6c7dfce20e2501273e
Sergey S <GeekCristiano@gmail.com>
2018-03-07 00:28:03 +0300

    Add example using of shutil module

--- Copy, rename files.py
+++ Copy, rename files.py
@@ -1,8 +1,10 @@
 # Lets' learn how copying file in python
 
+import shutil
 from os import path
 
-def get_info(file_name):
+
+def copy_file(file_name):
     if path.exists(file_name) and path.isfile(file_name):
         file_path = path.realpath(file_name)
 
@@ -10,4 +12,25 @@ def get_info(file_name):
         print("Path to file:", head)
         print("File name:", tail)
 
-get_info("file.txt")
+        # full path to our backup of source file
+        dst = file_path + ".bak"
+        # get copy only with content of original file
+        # shutil.copy(src=file_path, dst=dst)
+        # get copy with metada of original file
+        shutil.copystat(src=file_path, dst=dst)
+
+        return True if path.exists(dst) else False
+    else:
+        print("Incorrect input!")
+        return False
+
+
+def main():
+    if copy_file("file.txt") == True:
+        print("File successfully copied.")
+    else:
+        print("An error occurred while copying.")
+
+
+if __name__ == "__main__":
+    main()
