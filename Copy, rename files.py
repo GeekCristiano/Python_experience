977e0bc4f13b5b2f9fbafbbc3f648cda5a334566
Sergey S <GeekCristiano@gmail.com>
2018-03-06 23:59:27 +0300

    Add example using with os.path in python

--- Copy, rename files.py
+++ Copy, rename files.py
@@ -1,2 +1,13 @@
-
 # Lets' learn how copying file in python
+
+from os import path
+
+def get_info(file_name):
+    if path.exists(file_name) and path.isfile(file_name):
+        file_path = path.realpath(file_name)
+
+        head, tail = path.split(file_path)
+        print("Path to file:", head)
+        print("File name:", tail)
+
+get_info("file.txt")
