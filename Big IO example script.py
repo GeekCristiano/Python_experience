77b4b68ae2200fecd415b5a07829ae4a7b1adfc2
Sergey S <GeekCristiano@gmail.com>
2018-03-09 00:24:04 +0300

    Initial commit to big IO example

---
+++ Big IO example script.py
@@ -0,0 +1,8 @@
+# In this script I create a test directory, and inside it is a file.
+# In the test file, I record some data.
+# Next, I rename the test directory and the file.
+# After I copy the file.
+# Finally I delete the created directory and all the files inside.
+
+
+print("Soon there will be a lot of code. . .")
