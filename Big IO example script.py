5dbe52d0f7044e59576701c769fa324272054487
Sergey S <GeekCristiano@gmail.com>
2018-03-09 21:59:54 +0300

    Adds the function to creation item (now only a folder)

--- Big IO example script.py
+++ Big IO example script.py
@@ -7,7 +7,26 @@
 import os
 from os import path
 
-current_directory = path.realpath(path.curdir)
 
-os.mkdir(current_directory + os.sep + "test_directory")
-os.rmdir(current_directory + os.sep + "test_directory")
+def create_item(name):
+    # get script execution folder
+    current_directory = path.realpath(path.curdir)
+
+    # Try to create a new directory in the current folder
+    try:
+        os.mkdir(current_directory + os.sep + name)
+        return True
+    except OSError:
+        print("An error occurred while making new directory.")
+        return False
+
+
+def main():
+    if create_item("test_folder") is True:
+        print("New folder is successfully created!")
+    else:
+        print("An error occurred while making new directory.")
+
+
+if __name__ == "__main__":
+    main()
