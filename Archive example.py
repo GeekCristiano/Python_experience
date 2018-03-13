77a4d5a207eb2bcc1a1182b9366d85a206a6cce3
Sergey S <GeekCristiano@gmail.com>
2018-03-13 23:20:35 +0300

    Add piece of code to check creation an archive

--- Archive example.py
+++ Archive example.py
@@ -8,5 +8,11 @@ def main():
     full_current_path = os.path.realpath(os.path.curdir)
     shutil.make_archive("test_archive", "zip", full_current_path)
 
+    if (os.path.exists(os.path.realpath("test_archive.zip")) is True):
+        print("The archive was successfully created.")
+    else:
+        print("An error occurred while creating the archive.")
+
+
 if __name__ == "__main__":
     main()
