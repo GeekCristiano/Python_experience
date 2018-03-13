ab31abeca0fd28b679ac49fa5fd2b307c643f345
Sergey S <GeekCristiano@gmail.com>
2018-03-13 23:12:19 +0300

    Add simple example of using archive in python

--- Archive example.py
+++ Archive example.py
@@ -1,3 +1,12 @@
 # Python ZIP file
 
-print("Some code will appear here soon... ")
+import os
+import shutil
+
+
+def main():
+    full_current_path = os.path.realpath(os.path.curdir)
+    shutil.make_archive("test_archive", "zip", full_current_path)
+
+if __name__ == "__main__":
+    main()
