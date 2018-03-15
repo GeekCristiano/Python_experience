7676c704d18bf7c4e90703a2b251a9e50c1d5e55
Sergey S <GeekCristiano@gmail.com>
2018-03-15 23:46:21 +0300

    Add example of using minidom library

--- XML parsing.py
+++ XML parsing.py
@@ -1,11 +1,19 @@
-
 # let's study how work with XML in python
 
 import xml.dom.minidom
 
+
 def main():
-    print("Some piece of code will appear tomorrow. . .")
+    # load and parse source xml data
+    doc = xml.dom.minidom.parse("data.xml")
+
+    employees_list = doc.getElementsByTagName("employee")
+
+    print("The company has %d employees." % employees_list.length)
+    # print information about each employee
+    for employee in employees_list:
+        print(employee.toxml())
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
