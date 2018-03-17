3583d283264ccac6074e1812c0bca11693097086
Sergey S <GeekCristiano@gmail.com>
2018-03-17 23:54:21 +0300

    Add example of how create new xml tag and add to existing tree

--- XML parsing.py
+++ XML parsing.py
@@ -25,6 +25,27 @@ def main():
         name_employee = employee[0].text
         print(name_employee)
 
+    # creating XML Node and filling oft subtags
+    new_employee_tag = doc.createElement("employee")
+    new_employee_name_tag = doc.createElement("name")
+    new_employee_email_tag = doc.createElement("email")
+
+    new_employee_name_field = doc.createTextNode("John Smith")
+    new_employee_email_field = doc.createTextNode("SmithJ@gmail.com")
+
+    new_employee_name_tag.appendChild(new_employee_name_field)
+    new_employee_email_tag.appendChild(new_employee_email_field)
+
+    new_employee_tag.appendChild(new_employee_name_tag)
+    new_employee_tag.appendChild(new_employee_email_tag)
+
+    print("Information about the new employee:")
+    print(new_employee_tag.toprettyxml())
+
+    # Add new employee to company
+    doc.childNodes[0].appendChild(new_employee_tag)
+    print("Now in company works {} employees.".format(doc.getElementsByTagName("employee").length))
+
 
 if __name__ == "__main__":
     main()
