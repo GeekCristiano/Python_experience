4ebba24509b2953b190338df643d38f35e141642
Sergey S <GeekCristiano@gmail.com>
2018-03-19 23:38:25 +0300

    Add example of working with json object in python

--- json parsing.py
+++ json parsing.py
@@ -5,9 +5,13 @@ import json
 
 def main():
     json_data = '{"name": "John", "age": "27"}'
-    python_obj = json.loads(json_data)
-    print(python_obj)
-    print(type(python_obj))
+    python_json_obj = json.loads(json_data)
+    print(python_json_obj)
+    print(type(python_json_obj))
+
+    # after parsing json_data we have the dictionary structure
+    for key in python_json_obj.keys():
+        print(python_json_obj[key])
 
 
 if __name__ == "__main__":
