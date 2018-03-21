c7f134b573979958d0fb8c0673cfc0ec1010e458
Sergey S <GeekCristiano@gmail.com>
2018-03-21 23:19:37 +0300

    Add example of test data

--- Distribution of data.py
+++ Distribution of data.py
@@ -1,21 +1,30 @@
 # We solve the following problem:
 # There is a file for 4000 lines. It is necessary to make 40 files of 100 lines.
-import random
 
 
 def main():
     # create source data set
-    try:
-        source_file = open("source_file.txt", "w")
+    # try:
+    #     source_file = open("source_file.txt", "w")
+    #
+    #     # generate random data and write to source file
+    #     for elem in range(4000):
+    #         value = random.randint(10000000, 19999999)
+    #         source_file.write("{}\n".format(value))
+    #
+    #     source_file.close()
+    # except IOError:
+    #     print("An error occurred while opening the file.")
+    source_file = open("source_file.txt", "r")
 
-        # generate random data and write to source file
-        for elem in range(4000):
-            value = random.randint(10000000, 19999999)
-            source_file.write("{}\n".format(value))
+    # read and parse all data from source file
+    list = source_file.readlines()
 
-        source_file.close()
-    except IOError:
-        print("An error occurred while opening the file.")
+    for ind, value in enumerate(list):
+        if (ind + 1) % 40 == 0:
+            print(int(value))
+            print(int(ind))
+    source_file.close()
 
 
 if __name__ == "__main__":
