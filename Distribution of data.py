d425509cc73a01f1594a9eda26b722c9e7619843
Sergey S <GeekCristiano@gmail.com>
2018-03-21 22:29:20 +0300

    Initial commit to solve a real task

---
+++ Distribution of data.py
@@ -0,0 +1,9 @@
+# We solve the following problem:
+# There is a file for 4000 lines. It is necessary to make 40 files of 100 lines.
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
