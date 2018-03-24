94594f4f6de00ab94d30ffb4134c4e2cd4f43441
Sergey S <GeekCristiano@gmail.com>
2018-03-24 14:41:12 +0300

    Add code to creation and filling data to source file

--- Distribution of data.py
+++ Distribution of data.py
@@ -2,6 +2,7 @@
 # There is a file for 4000 lines. It is necessary to make 40 files of 100 lines.
 
 import os
+from random import randint
 
 
 def main():
@@ -17,43 +18,46 @@ def main():
     except OSError:
         print("An error occurred while making new directory.")
 
+    # create source data set
 
-        # create source data set
+    source_file_name = "dataset.txt"
+    source_file_full_path = test_dir_full_path + os.sep + source_file_name
+
+    try:
+        source_file = open(source_file_full_path, "w")
+
+        # generate random data and write to source file
+
+        for i in range(4000):
+            value = randint(10000000, 19999999)
+            source_file.write("{}\n".format(value))
+
+        source_file.close()
+
+    except IOError:
+        print("Something went wrong. . .")
 
 
-# try:
-#     source_file = open("source_file.txt", "w")
+# source_file = open("source_file.txt", "r")  # read and parse all data from source file
+# list = source_file.readlines()
 #
-#     # generate random data and write to source file
-#     for elem in range(4000):
-#         value = random.randint(10000000, 19999999)
-#         source_file.write("{}\n".format(value))
+# file_name_template = "source_file_{}.txt"
+# count = 0
 #
-#     source_file.close()
-# except IOError:
-#     print("An error occurred while opening the file.")
-source_file = open("source_file.txt", "r")
-
-# read and parse all data from source file
-list = source_file.readlines()
-
-file_name_template = "source_file_{}.txt"
-count = 0
-
-for ind, value in enumerate(list):
-
-    if (ind + 1) % 40 == 0:
-        count = count + 1
-        start_index = (ind + 1) - 40
-        print(start_index)
-
-        file = open(file_name_template.format(count), "w")
-
-        for i in range(40):
-            file.write(list[start_index + i])
-
-        file.close()
-source_file.close()
+# for ind, value in enumerate(list):
+#
+#     if (ind + 1) % 40 == 0:
+#         count = count + 1
+#         start_index = (ind + 1) - 40
+#         print(start_index)
+#
+#         file = open(file_name_template.format(count), "w")
+#
+#         for i in range(40):
+#             file.write(list[start_index + i])
+#
+#         file.close()
+# source_file.close()
 
 if __name__ == "__main__":
     main()
