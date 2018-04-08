799fea4cb3844342fefaf5f73fed593788db597a
Sergey S <GeekCristiano@gmail.com>
2018-04-08 22:46:25 +0300

    Add more example with sets

--- Sets.py
+++ Sets.py
@@ -2,7 +2,12 @@
 
 def main():
     set1 = {1, 19, 45, 78}
-    set2 = set(5, 78, 9, 6, 19)
+    numbers = [5, 78, 9, 6, 19]
+    set2 = set(numbers)
+    print("Difference between first and second set:", set1.difference(set2))
+    print("Difference another way:", set1 - set2)
+    print("Difference another way:", set2 - set1)
+    print("Difference between second and first set:", set2.difference(set1))
 
 
 if __name__ == "__main__":
